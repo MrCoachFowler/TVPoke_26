3f3d50f5a580b0495b3504a719cfd89f0cc335bb
elias-pie <elias8xv4@proton.me>
2026-03-06 11:49:07 -0700

    Revise Selection Screen

--- SelectScreen.py
+++ SelectScreen.py
@@ -4,7 +4,7 @@ from TVPoke.helper import getAllPokemonNames
 
 class SelectScreen(Screen):
     def __init__(self, window):
-        super().__init__(window, (4,50,3))
+        super().__init__(window, (39, 115, 52))
         self.state = {
             "trainerIndex": 0,
             "selectedPoke": [[], []],
@@ -13,14 +13,18 @@ class SelectScreen(Screen):
             "pokemonShowing": [],
             "hasNextPage": False,
             "hasPrevPage": False,
-            "goTo": ""
+            "goTo": "",
+            "confirmed": False,
+            "buttonText": "Confirm",
+            "buttonColor": [(0,0,0), (255, 255, 255)]
         }
         self.updatePokemonShowing()
 
 
     def elementsToDisplay(self):
         self.elements = [ 
-            Label((25, 95), 50, 10, "Choose your pokemon!\nPlayer " + str(self.state["trainerIndex"] + 1), 24)
+            Label((25, 95), 50, 10, "Choose your pokemon!\nPlayer " + str(self.state["trainerIndex"] + 1), 24),
+            ConfirmButton((75, 5), 30, 8, self.state["buttonText"], self.state["buttonColor"][0], self.state["buttonColor"][1])
         ]
         itemsPerRow = 3
         rowsOfItems = 3
@@ -52,9 +56,10 @@ class SelectScreen(Screen):
         for p in self.state["selectedPoke"][self.state["trainerIndex"]]:
             yCoord += 100/4
             self.elements.append(SelectedPokeImage((75, yCoord), p, 20, 100/4))
-        if len(self.state["selectedPoke"][self.state["trainerIndex"]]) == 3:
+        if len(self.state["selectedPoke"][self.state["trainerIndex"]]) == 3 and self.state["confirmed"] == True:
             self.waitForNextFrame = 2
-            self.elements.append(Label((75, 90), 50, 20, "Choosing Complete!", 30))
+            self.elements.append(Label((75, 95), 50, 20, "Choosing Complete!", 30))
+            self.state["confirmed"] = False
             if self.state["trainerIndex"] == 1:
                 self.state["goTo"] = "BATTLE"
             else:
@@ -74,16 +79,24 @@ class SelectScreen(Screen):
                 break
         self.state["pokemonShowing"] = pokeShowing
                 
-
-
-
 class PokeImage(Image):
     def __init__(self, pos, name, width, height):
         self.name = name
         super().__init__(pos, width, height, './TVPoke/Pokemon/imgs/' + name + '.png')
 
     def onClick(self, screen):
-        screen.state["selectedPoke"][screen.state["trainerIndex"]].append(self.name)
+        if (screen.state["trainerIndex"] == 0 and len(screen.state["selectedPoke"][0]) != 3):
+            screen.state["selectedPoke"][screen.state["trainerIndex"]].append(self.name)
+            screen.state["buttonText"] = 'Confirm'
+            screen.state["buttonColor"][1] = (255, 255, 255)
+        elif (screen.state["trainerIndex"] == 1 and len(screen.state["selectedPoke"][1]) != 3):
+            screen.state["selectedPoke"][screen.state["trainerIndex"]].append(self.name)
+            screen.state["buttonText"] = 'Confirm'
+            screen.state["buttonColor"][1] = (255, 255, 255)
+        else:
+            screen.state["buttonText"] = 'Max of 3 is allowed!'
+            screen.state["buttonColor"][1] = (245, 66, 69)
+
 
 class SelectedPokeImage(Image):
     def __init__(self, pos, name, width, height):
@@ -92,10 +105,13 @@ class SelectedPokeImage(Image):
 
     def onClick(self, screen):
         screen.state["selectedPoke"][screen.state["trainerIndex"]].remove(self.name)
+        screen.state["buttonText"] = 'Confirm'
+        screen.state["buttonColor"][1] = (255, 255, 255)
+
 
 class BackButton(Image):
     def __init__(self):
-        super().__init__((5, 85), 10, 10, './imgs/BkBttn.png')
+        super().__init__((5, 85), 10, 13, './imgs/BkBttn.png')
 
     def onClick(self, screen):
         screen.state["pageNum"] -= 1
@@ -103,11 +119,22 @@ class BackButton(Image):
 
 class ForwardButton(Image):
     def __init__(self):
-        super().__init__((45, 85), 10, 10, './imgs/FwdBttn.png')
+        super().__init__((45, 85), 10, 13, './imgs/FwdBttn.png')
 
     def onClick(self, screen):
         screen.state["pageNum"] += 1
         screen.updatePokemonShowing()
 
+class ConfirmButton(Button):
+    def __init__(self, centerXY, width, height, text, textColorRGB=(0,0,0), backColorRGB=(255,255,255)):
+        super().__init__(centerXY, width, height, text, textColorRGB, backColorRGB)
+
+    def onClick(self, screen):
+        if len(screen.state["selectedPoke"][0]) == 3:
+            screen.state["confirmed"] = True
+            print('ConfirmedPokemon')
+        else:
+            screen.state["buttonText"] = 'You need 3 pokes'
+            screen.state["buttonColor"][1] = (245, 66, 69)
     
         
